ae375d08dad773281ca1df7184a17c3abd43efdd
konum <darkkonum@gmail.com>
2015-10-16 09:51:48 -0300

    Problem 1

---
+++ src/problem1/Problem1.java
@@ -0,0 +1,19 @@
+package problem1;
+
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+//Find the sum of all the multiples of 3 or 5 below 1000.
+@FunctionalInterface 
+interface Operate<T>{
+	public T operate(T op1, Predicate op2);
+}
+
+public class Problem1 {
+	public static void main(String[] args) {
+		System.out.println(IntStream.range(0, 1000).filter(val -> (val % 3==0 || val % 5==0)).sum());
+	}
+}
